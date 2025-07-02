702aa9ffa3dc9f81403f36a71cd740b6d19d2cb4
shunquan.wang <shunquan.wang@ly.com>
2025-07-02 15:46:59 +0800

    refactor: remove unused defaultErrorLoggerName constant from logger constants

--- packages/logger/src/constant/constant.ts
+++ packages/logger/src/constant/constant.ts
@@ -2,6 +2,4 @@ import { LogLevel } from './log-level.js';
 
 export const defaultLoggerName = 'hps-logger';
 
-export const defaultErrorLoggerName = 'hps-logger-error';
-
 export const defaultLogLevel = LogLevel.Info;
